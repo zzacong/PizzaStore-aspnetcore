5a6aa2f13115f2555f3a839ea9db93b9cc84045a
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-07-28 16:20:26 +1000

    sqlite db

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ using PizzaStore.DB;
 using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+// builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDb>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
   c.SwaggerDoc("v1", new OpenApiInfo
